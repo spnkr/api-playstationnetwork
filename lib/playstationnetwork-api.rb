f59659196785f6c46449eac8f509f97247494bde
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2016-03-17 15:51:59 +0000

    v.0.0.1 working of the API with profile, games, trophies and trophy group

---
+++ lib/playstationnetwork-api.rb
@@ -0,0 +1,9 @@
+require 'playstationnetwork/version'
+require 'httparty'
+
+module PlayStationNetwork
+
+  Dir[File.dirname(__FILE__) + '/playstationnetwork/*.rb'].each do |file|
+    require file
+  end
+end
---
+++ lib/playstationnetwork/version.rb
@@ -0,0 +1,3 @@
+module PlayStationNetwork
+  VERSION = '0.0.1'
+end
