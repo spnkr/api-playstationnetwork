5f1e133f54bcb6797ad458338801f732cb69fb63
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2016-04-21 23:43:04 +0100

    bump version and add changelog

--- lib/playstationnetwork/version.rb
+++ lib/playstationnetwork/version.rb
@@ -1,3 +1,9 @@
 module PlayStationNetwork
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
+
+  # Changelog
+  #
+  # v0.0.1 - Initial
+  # v0.0.2 - Added Trophy Information
+  # v0.0.3 - Added Trophies offset
 end
